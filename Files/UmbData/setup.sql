2cad41bd4bf5a9edf2591f1876995a1be88aa11e
Carl Sargunar <carl@sargunar.com>
2023-01-19 00:14:13 +0000

    update

--- Files/UmbData/setup.sql
+++ Files/UmbData/setup.sql
@@ -4,8 +4,10 @@ GO
 IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'UmbracoDb')
 BEGIN
 
-    CREATE DATABASE [UmbracoDb];
-
+    CREATE DATABASE [UmbracoDb] ON 
+    ( FILENAME = N'/var/opt/sqlserver/Umbraco.mdf' ),
+    ( FILENAME = N'/var/opt/sqlserver/Umbraco_log.ldf' )
+    FOR ATTACH
 END;
 GO
 
